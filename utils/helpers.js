06a7f3c79311c16046e646392b38cf07b75aebb8
Aleksandr YUrkovskiy <godlikedesigner@gmail.com>
2019-03-10 20:56:02 +0500

    add func randomString

--- utils/helpers.js
+++ utils/helpers.js
@@ -8,4 +8,16 @@ helpers.parseJsonToObject = (str) => {
     }
 };
 
+helpers.randomStr = (len) => {
+    len = typeof (len) !== 'number' ? 0 : len;
+    const chars = 'abcdefghijklnopqrstuwyz0123456789ABCDEFGHIJKLNOPQRSTUWYZ';
+    let res = '';
+    for(let i = 0; i <= len; i++)
+    {
+        let randomChar = chars.charAt(Math.floor(Math.random() * chars.length));
+        res+=randomChar;
+    }
+
+    return res;
+};
 module.exports = helpers;
